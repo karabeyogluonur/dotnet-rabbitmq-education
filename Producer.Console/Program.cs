28b1848d7ecfbdc8cedc3c1cd0465c115374283a
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-25 19:13:26 +0300

    Rabbitmq connection created.

---
+++ Producer.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Producer.Console;
+using RabbitMQ.Client;
+
+Console.WriteLine("[*] Rabbitmq producer project started.");
+
+
+
+
+IConnection rabbitMQConnection = RabbitMQConnection.CreateConnection();
+
+if (rabbitMQConnection.IsOpen)
+    Console.WriteLine("[**] Rabbitmq connection is open.");
+else
+    Console.WriteLine("[***] Rabbitmq connection is close.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
+
---
+++ Producer.Console/RabbitMQConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Producer.Console
+{
+	public static class RabbitMQConnection
+	{
+		public static IConnection CreateConnection()
+		{
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri($"{RabbitMQDefaults.Prefix}" +
+                              $"{RabbitMQDefaults.Username}:{RabbitMQDefaults.Password}@" +
+                              $"{RabbitMQDefaults.Host}:{RabbitMQDefaults.Port}", UriKind.RelativeOrAbsolute)
+            };
+
+            return connectionFactory.CreateConnection();
+		}
+
+	}
+}
+
