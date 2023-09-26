55a128d0a043d6c45176c67532a0dc480ee48c32
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-27 00:27:11 +0300

    Added consumer receive.

--- Consumer.Console/Program.cs
+++ Consumer.Console/Program.cs
@@ -1,5 +1,25 @@
-﻿
+﻿using System.Text;
+using Consumer.Console;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+IConnection connection = RabbitMQConnection.CreateConnection();
+IModel channel = connection.CreateModel();
 
 
+string publishTestQueueName = "publish_test_queue";
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[**] Received : {message} ");
+};
+
+channel.BasicConsume(queue: publishTestQueueName,
+                     autoAck: true,
+                     consumer: consumer);
 
 Console.ReadKey();
