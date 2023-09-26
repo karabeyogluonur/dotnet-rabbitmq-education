3a519d388b91f94a7212433d27d332e5144345fe
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-27 02:04:10 +0300

    Order model created.

---
+++ OrderProducer.Console/Models/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OrderProducer.Console.Models
+{
+	public class OrderModel
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; } = String.Empty;
+		public string LastName { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Phone { get; set; } = String.Empty;
+
+        public List<ProductModel> Products { get; set; }
+
+	}
+}
+
---
+++ OrderProducer.Console/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OrderProducer.Console.Models
+{
+	public class ProductModel
+	{
+		public string Name { get; set; }
+		public int Quantity { get; set; }
+	}
+}
+
